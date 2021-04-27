7135b8a99458142f118c282c875fd178cefbf4e2
JasonMatthewsNexusPoint <70138044+JasonMatthewsNexusPoint@users.noreply.github.com>
2021-04-27 10:17:45 +0100

    Temp commit

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
 const express = require('express');
 const axios = require('axios');
 const moment = require('moment');
+// const ObjectsToCsv = require('objects-to-csv');
+const fs = require('fs')
+
 const ObjectsToCsv = require('objects-to-csv');
 
+let stringify = require('csv-stringify');
+
 const app = express();
-const port = 8000;
+const port = 7999;
 
 async function dates(){
   let arr = [];
   let obj = {};
-    for(let i=0; i<=4; i++){
-      let date = (moment().subtract(i, "days").format('YYYY-MM-D'));
-      await axios.get('https://api.ratesapi.io/api/' + date + '?base=GBP')
-      .then(response => {
-        obj['date']  = date;
-        obj['rates'] = response.data.rates
-        arr.push(obj);
-      })
-    }
 
-    const csv = new ObjectsToCsv(arr, { allColumns: true });
-    await csv.toDisk('./test.csv');
+  for(let i=0; i<=4; i++){
+    let date = (moment().subtract(i, "days").format('YYYY-MM-D'));
+    await axios.get('https://api.ratesapi.io/api/' + date + '?base=GBP')
+    .then(response => {
+      obj['date'] = response.data.date;
+      obj['date']["rates"] = response.data.rates;
+    })
+    
+    arr.push(obj);
+    console.log(arr);
+    (async () => {
+      const csv = new ObjectsToCsv(arr);
+     
+      // Save to file:
+      await csv.toDisk('./test.csv', { append: true ,  header: false, allColumns: true });
+     
+      // Return the CSV file as string:
+      // console.log(await csv.toString());
+    })();
+
+    // stringify(arr, {
+    //   columns: ['date']
+    //   }, function (err, output) {
+    //     fs.appendFileSync('./someData.csv', output,function(err, result) {
+    //       if(err) console.log('error', err);
+    //     });
+    //   }
+    // )
+    console.log(obj);
+  } 
 }
 
 dates();
